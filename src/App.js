e4bf1e9894ba4285ad1780bb4db35b3122dcefca
Daniel Eduardo Medina <danielmedinacs97@gmail.com>
2021-11-21 16:08:19 -0800

    Added loading  text while fetch occuring and div for results

--- src/App.js
+++ src/App.js
@@ -3,40 +3,31 @@ import './App.css';
 import Navbar from './resources/components/navbar/Navbar';
 import ActivityForm from './resources/components/activity-form/ActivityForm';
 import Results from './resources/components/results/Results';
-import { useState, useEffect, useRef } from 'react';
+import { useState } from 'react';
 
 function App() {
   
-
-
-  var data = null;
-
   const [queryData, setQueryData] = useState(null);
-
-  const isMounted = useRef(false);
-  
-  useEffect(() => {
-    if(isMounted.current){
-    document.getElementById("activitySpan").textContent = "Hello World";
-    } else {
-      isMounted.current = true;
-    }
-  }, [queryData]);
-  
-
-let getQuery = async (queryURL) => {
-  data = await fetch(queryURL)
-  data =  await data.json();
-  console.log(data);
-  setQueryData(data);
-}
+  const [loading, setLoading] = useState(true);
+  const [madeQuery, setMadeQuery] = useState(false);
+ 
+  let getQuery = async (queryURL) => {
+    setLoading(true);
+    const response = await fetch(queryURL)
+    const data =  await response.json();
+    console.log(data);
+    const item = data;
+    console.log(item);
+    setQueryData(item);
+    setLoading(false);
+  }
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    setMadeQuery(true);
     let queryURL = "http://www.boredapi.com/api/activity/";
     let values = Array.from(document.getElementsByClassName("activity"));
     let isEmpty = true;
-    let name ="hello";
     for(let i in values){
       if(values[i].value !== ""){
         if(isEmpty){
@@ -47,25 +38,19 @@ let getQuery = async (queryURL) => {
         queryURL = queryURL + values[i].id +  '=' + values[i].value;
       }
     }
-    console.log(queryURL);
+    //console.log(queryURL);
     getQuery(queryURL);
 
   };
 
-
   return (
     <div className="App">
-      
       <div className="app-body">
         <Navbar/>
-
         <ActivityForm handleSubmit = {handleSubmit}/>
-
-        <Results/>
+        {!madeQuery ? <div> Your suggestion here: </div> : loading ? <div>Loading...</div>:<Results queryData={queryData}/>}
+        {/*loading  && madeQuery? <div>Loading...</div>:<Results queryData={queryData}/>*/}
       </div>
-
-      
-    
     </div>
   );
 }
--- src/resources/components/results/Results.js
+++ src/resources/components/results/Results.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import './Results.css'
 
-export default function Results() {
+export default function Results({queryData}) {
+    function capitalizeFirstLetter(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+      }
     return (
         <>
             <h1>Do This!</h1>
             <div className="results">
-                <div>Activity:<span className="label" id="activitySpan"></span> </div>
-                <div><span className="results" id="typeSpan">Type:</span> </div>
-                <div><span className="results" id="participantsSpan">Participants:</span> </div>
-                <div><span className="results" id="accessibilitySpan">Accessibility:</span> </div>
-                <div><span className="results" id="priceSpan">Price:</span> </div>
+                <div>Activity:<span className="label" id="activitySpan"> {queryData.activity}</span> </div>
+                <div><span className="results" id="typeSpan">Type: {capitalizeFirstLetter(queryData.type)}</span> </div>
+                <div><span className="results" id="participantsSpan">Participants: {queryData.participants}</span> </div>
+                <div><span className="results" id="accessibilitySpan">Accessibility: {queryData.accessibility}</span> </div>
+                <div><span className="results" id="priceSpan">Price: {queryData.price}</span> </div>
             </div>
         </>
     )
